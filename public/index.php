ceff4f53caaec92d5ad006e3c8a5cbcfc569813a
Sajjad Esmaeeli <s.sajjad.esmaeeli@gmail.com>
2023-01-12 13:26:00 +0330

    env file loaded in application - autoloading done - add helpers - add database configs file

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'hi';
+use Dotenv\Dotenv;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->safeLoad();
+
+echo 'Autoload is loaded successfully with env variables.';
