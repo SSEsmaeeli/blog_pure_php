ce7adf8e05a00d5b3b87ca1f04802ea35276d963
Sajjad Esmaeeli <s.sajjad.esmaeeli@gmail.com>
2023-01-12 13:54:05 +0330

    add basic router

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController
+{
+    public function index()
+    {
+        dd('Yes router is working and you came to controller!');
+    }
+}
---
+++ core/Router.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Core;
+
+use Exception;
+
+class Router
+{
+    private const CONTROLLER_BASE_PATH = 'App\\Http\\Controllers\\';
+
+    private array $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
+
+    public static function load($routeFile): static
+    {
+        $router = new static;
+
+        require $routeFile;
+
+        return $router;
+    }
+
+    /**
+      * @throws Exception
+      */
+     public function resolve(string $uri, string $method)
+     {
+         $uri = trim($uri, '/');
+
+         if(! array_key_exists($uri, $this->routes[$method])) {
+             abort(404, 'Sorry the page you are looking for is not available at the moment!');
+         }
+
+         $controllerNameAndMethod = $this->routes[$method][$uri];
+
+         return $this->callAction(
+             ...explode('@', $controllerNameAndMethod)
+         );
+     }
+
+     public function get($uri, $controllerAndAction): void
+     {
+         $this->routes['GET'][trim($uri, '/')] = $controllerAndAction;
+     }
+
+     public function post($uri, $controllerAndAction): void
+     {
+         $this->routes['POST'][trim($uri, '/')] = $controllerAndAction;
+     }
+
+     private function callAction(string $controller, string $action)
+     {
+         $controller = new (static::CONTROLLER_BASE_PATH.$controller);
+
+         if(! method_exists($controller, $action)) {
+             $controllerName = $controller::class;
+             throw new Exception("Sorry, {$action} is not available inside controller {$controllerName}.");
+         }
+
+         return $controller->$action();
+     }
+}
