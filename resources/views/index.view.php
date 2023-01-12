872319b7bdc4d11a02faff2ca45a489e26a65d2b
Sajjad Esmaeeli <s.sajjad.esmaeeli@gmail.com>
2023-01-12 17:07:16 +0330

    add authetincated user info

--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -1,5 +1,11 @@
 <?php require('layouts/header.view.php'); ?>
 
-    <h1>Home Page</h1>
+    <h1>Welcome</h1>
 
+    <?php if (isAuth()) { ?> <br>
+    name: <?= authUser()?->name ?> <br>
+    Family: <?= authUser()?->family ?> <br>
+    Username: <?= authUser()?->username ?> <br>
+    Registered At: <?= authUser()?->created_at ?> <br>
+    <?php } ?>
 <?php require('layouts/footer.view.php'); ?>
